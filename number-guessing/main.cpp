1cc05658b7d9870a1e2061dd323f411081e797f5
archivedghost001 <aryamuhammadkamil@gmail.com>
2024-11-03 16:01:41 +0700

    second and a half commit i guess

--- number-guessing/main.cpp
+++ number-guessing/main.cpp
@@ -9,7 +9,7 @@ int main()
   srand(time(0));
   int number = rand() % 99 + 1;
   string name;
-  int userInput;
+  int userInput = rand() % 99 + 1;
 
   cout << "Welcome to the dice game, if you lose you die" << endl;
   for (int i = 0; i <= 30; i++)
@@ -17,24 +17,26 @@ int main()
     cout << "-";
   }
   cout << endl;
+  while (true)
+  {
+    cout << "Enter your name: ";
+    cin >> name;
 
-  cout << "Enter your name: ";
-  cin >> name;
+    // cout << "Enter your number: ";
+    // cin >> userInput;
 
-  cout << "Enter your number: ";
-  cin >> userInput;
+    cin.ignore();
 
-  cin.ignore();
-
-  if (userInput < number) {
-    cout << name << " lost! AI number was " << number << endl;
+    if (userInput < number) {
+      cout << "You role "<< userInput << ". " << name << " lost! AI number was " << number << endl;
+    }
+    else if (userInput > number) {
+      cout << "You role "<< userInput << ". "  << name << " win! AI number was " << number << endl;
+    }
+    else {
+      cout << "You role "<< userInput << ". "  << name << " blocked! AI number was " << number << endl;
+    }
   }
-  else if (userInput > number) {
-    cout << name << " win! AI number was " << number << endl;
-  }
-  else {
-    cout << name << " blocked! AI number was " << number << endl;
-  }
-
+  
   return 0;
 }
